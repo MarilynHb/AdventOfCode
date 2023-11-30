5e8ac7117f32012ea393ab571c763831bf54ad8e
Marilyn Haber <mha@logisoft.io>
2023-11-30 13:28:18 +0200

    Add project files.

---
+++ AdventOfCode/2022/CountCalories_Day1_2022.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace AdventOfCode;
+
+public class CountCalories_Day1_2022
+{
+    string projectPath = Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.FullName ?? throw new NullReferenceException();
+    const string year = "2022";
+    internal void TopElfWithMaxTotalCalories(int take)
+    {
+        var path = $"{projectPath}\\{year}\\Input\\{year}_Day1.txt";
+
+        IEnumerable<string> lines = File.ReadAllLines(path, Encoding.UTF8);
+        if (!lines.Any()) 
+        {
+            Console.WriteLine("Empty File");
+            return;
+        }
+
+        int id = 0;
+        IList<Elf> elves = [new() { Id = id }];
+        foreach (var line in lines)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                ++id;
+                elves.Add(new Elf { Id = id });
+                continue;
+            }
+            var elf = elves[id].TotalCalories += long.Parse(line);
+        }
+        var top = (from x in elves
+                   orderby x.TotalCalories descending
+                   select x.TotalCalories)
+                  .Take(take)
+                  .ToArray();
+
+        Console.WriteLine(top.Sum());
+    }
+
+    public class Elf
+    {
+        public int Id { get; set; }
+        public long TotalCalories { get; set; }
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace AdventOfCode;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        new CountCalories_Day1_2022().TopElfWithMaxTotalCalories(1);
+    }
+}
